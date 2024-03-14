06bf564188328657bfca4f6ab42b66002d4c06ab
Nanda6618 <163292124+Nanda6618@users.noreply.github.com>
2024-03-14 09:00:43 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='piexplorers',  # Replace 'piexplorers' with your desired package name
+    version='0.1',  # Specify your package version
+    packages=find_packages(),  # Automatically find packages in the project directory
+    install_requires=[],  # Add any dependencies your package requires
+)
